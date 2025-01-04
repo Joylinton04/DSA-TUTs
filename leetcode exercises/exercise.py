9894f8ede7b8600c1659c4312276035e857ec850
Joylinton04 <adujoy05@gmail.com>
2025-01-04 11:11:22 +0000

    roman_to_int

---
+++ leetcode exercises/exercise.py
@@ -0,0 +1,18 @@
+class Solution:
+    def RomanToInt(self, x:str):
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        prev_value = 0
+        
+        for char in reversed(x):
+            current_value = roman_values[char]
+            if current_value < prev_value:
+                result -= current_value
+            else:
+                result += current_value
+            prev_value = current_value
+            
+        return result
+    
+solution = Solution()
+print(solution.RomanToInt("IX"))
