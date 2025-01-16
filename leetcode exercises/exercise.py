019de4987314346da91f66a01d0ad388de2d148f
Joylinton04 <adujoy05@gmail.com>
2025-01-16 16:35:37 +0000

    refactor

--- leetcode exercises/exercise.py
+++ leetcode exercises/exercise.py
@@ -14,5 +14,17 @@ class Solution:
             
         return result
     
+    def IntToRoman(self, x):
+        roman_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+        result = ""
+        
+        for value, roman in roman_map:
+            while x >= value:
+                result += roman
+                x -= value
+        return result
+    
+    
+    
 solution = Solution()
-print(solution.RomanToInt("IX"))
+print(solution.IntToRoman(0))
