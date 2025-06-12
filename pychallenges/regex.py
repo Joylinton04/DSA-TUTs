2b23db92541ebb4601c035e502ac510eadaa3e0d
Joylinton04 <adujoy05@gmail.com>
2025-06-12 20:07:02 +0000

    py regex basics

--- pychallenges/regex.py
+++ pychallenges/regex.py
@@ -1,16 +1,28 @@
 import re
 
-pattern = "[a-zA-Z0-9]+@[a-z](.com)"
 
 # this method substitutes the pattern with nothing 
 # re.sub(r"[^a-zA-Z0-9\s.,@]", "")
 
-word = "example05@gmail.com"
+def checkEmail():
+    pattern = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|net)"
 
-if(re.search(pattern, word)):
-    print("Valid")
-else:
-    print("Invalid")
+    email = input("Enter your email: ")
+
+    if(re.search(pattern, email)):
+        print("Valid")
+    else:
+        print("Invalid"),
 
 
+def formatPhone():
+    phone_pattern = r"(\d{3})(\d{4})(\d{4})"
+    phone = input("Enter your phone: ")
+    formatted = re.sub(phone_pattern, r"\1-\2-\3", phone)
+    
+    if(formatted):
+        print(formatted)
+    else:
+        print("Invalid")
 
+formatPhone()
