24d18f0174bafd68a2ac963f12751c12a6ea86c3
Joylinton04 <adujoy05@gmail.com>
2025-10-21 11:35:49 +0000

    double linked list

---
+++ Linked List/DoubleLL.py
@@ -0,0 +1,66 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+        
+
+class DoubleLList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+    
+    def createDLL(self, value):
+        node = Node(value)
+        self.head = node
+        self.tail = node
+        node.next = None
+        node.prev = None
+        return "The DLL has been created"
+    
+    def insertDLL(self,value,location):
+        if self.head == None:
+            print("This node cannot be inserted")
+        else:
+            newNode = Node(value)
+            if location == 0:
+                newNode.prev = None
+                newNode.next = self.head
+                self.head.prev = newNode
+                self.head = newNode
+            elif location == 1:
+                newNode.next = None
+                newNode.prev = self.tail
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode =tempNode.next
+                    index += 1
+                newNode.next = tempNode.next
+                newNode.prev = tempNode
+                newNode.next.prev = newNode
+                tempNode.next = newNode
+                
+            
+            
+    
+    
+Dll = DoubleLList()
+print(Dll.createDLL(5))
+print([node.value for node in Dll])
+Dll.insertDLL(4,0)
+# Dll.insertDLL(3,0)
+Dll.insertDLL(2,0)
+Dll.insertDLL(1,0)
+Dll.insertDLL(6,1)
+Dll.insertDLL(3,2)
+print([node.value for node in Dll])
