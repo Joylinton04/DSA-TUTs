81d09e27c6d61a7e5f235c69a4b8aba4fc70d127
Joylinton04 <adujoy05@gmail.com>
2025-11-18 08:42:34 +0000

    queue with py collection queue

---
+++ queue/deque.py
@@ -0,0 +1,13 @@
+# using py deque class to create FIFO queue
+from collections import deque
+
+customQueue = deque(maxlen=3)
+print(customQueue)
+
+customQueue.append(1)
+customQueue.append(2)
+customQueue.append(3)
+
+print(customQueue)
+customQueue.popleft()
+print(customQueue)
