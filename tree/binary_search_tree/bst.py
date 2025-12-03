d57fbafcdb75e539fb77b2d3005dee16548d9edd
Joylinton04 <adujoy05@gmail.com>
2025-12-03 09:19:58 +0000

    feat: Add tree traversal, search, and deletion methods to Binary Search Tree.

--- tree/binary_search_tree/bst.py
+++ tree/binary_search_tree/bst.py
@@ -26,7 +26,79 @@ class BinarySearchTree:
                 currentNode.right = BSTNode(nodeValue)
             else:
                 self._insert(nodeValue, currentNode.right)
+        return "Value Inserted"
         
+    def preOrderTraversal(self, rootNode):
+        if not rootNode:
+            return
+        print(rootNode.data)
+        self.preOrderTraversal(rootNode.left)
+        self.preOrderTraversal(rootNode.right)
+
+    def inOrderTraversal(self, rootNode):
+        if not rootNode:
+            return
+        self.inOrderTraversal(rootNode.left)
+        print(rootNode.data)
+        self.inOrderTraversal(rootNode.right)
+
+    def postOrderTraversal(self, rootNode):
+        if not rootNode:
+            return
+        self.postOrderTraversal(rootNode.left)
+        self.postOrderTraversal(rootNode.right)
+        print(rootNode.data)
+
+    def levelOrderTraversal(self, rootNode):
+        if not rootNode:
+            return
+        queue = []
+        queue.append(rootNode)
+        while queue:
+            currentNode = queue.pop(0)
+            print(currentNode.data)
+            if currentNode.left:
+                queue.append(currentNode.left)
+            if currentNode.right:
+                queue.append(currentNode.right)
+
+    def search(self, rootNode, nodeValue):
+        if not rootNode:
+            return "Value Not Found"
+        if rootNode.data == nodeValue:
+            return "Value Found"
+        if nodeValue < rootNode.data:
+            return self.search(rootNode.left, nodeValue)
+        return self.search(rootNode.right, nodeValue)
+
+    def minValueNode(self, currentNode):
+        while currentNode.left is not None:
+            currentNode = currentNode.left
+        return currentNode
+
+    def deleteNode(self, rootNode, nodeValue):
+        if not rootNode:
+            return rootNode
+        if nodeValue < rootNode.data:
+            rootNode.left = self.deleteNode(rootNode.left, nodeValue)
+        elif nodeValue > rootNode.data:
+            rootNode.right = self.deleteNode(rootNode.right, nodeValue)
+        else:
+            if rootNode.left is None:
+                temp = rootNode.right
+                rootNode = None
+                return temp
+            elif rootNode.right is None:
+                temp = rootNode.left
+                rootNode = None
+                return temp
+            temp = self.minValueNode(rootNode.right)
+            rootNode.data = temp.data
+            rootNode.right = self.deleteNode(rootNode.right, temp.data)
+        return rootNode
+
+    def delete(self, nodeValue):
+        self.deleteNode(self.root, nodeValue)
 
 
 BST = BinarySearchTree()
@@ -37,3 +109,15 @@ BST.insert(3)
 BST.insert(7)
 BST.insert(12)
 BST.insert(20)
+
+print("Pre Order Traversal")
+BST.preOrderTraversal(BST.root)
+
+print("In Order Traversal")
+BST.inOrderTraversal(BST.root)
+
+print("Post Order Traversal")
+BST.postOrderTraversal(BST.root)
+
+print("Level Order Traversal")
+BST.levelOrderTraversal(BST.root)
